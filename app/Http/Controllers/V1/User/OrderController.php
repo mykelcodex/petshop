a1b90c0e661dd060872e8be12a05bcfb5f2b88fc
mykelcodex <michealoke39@yahoo.com>
2022-02-16 22:49:06 +0100

    completed all users route

--- app/Http/Controllers/V1/User/OrderController.php
+++ app/Http/Controllers/V1/User/OrderController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers\v1\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+
+	use ApiResponser;
+
 	public function __construct(){
 		$this->middleware('auth.user');
 	}
 
+
+	/**
+	 * Get user orders
+	 */
+	public function getOrders(){
+		$orders = Order::where('user_id', auth()->id())->paginate(5);
+		return $this->successResponse($orders);
+	}
 
 }

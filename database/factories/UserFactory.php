2d736fda55034e531deea32b3fba71916b2417ef
mykelcodex <michealoke39@yahoo.com>
2022-02-24 23:57:41 +0100

    update readme

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'firstname' => $this->faker->firstName(),
+            'lastname' => $this->faker->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'address' => $this->faker->streetAddress(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
         ];
     }
 
